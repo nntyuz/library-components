b4f560c72747f99a5a94c43c7bb3896ffca3074c
nnuyz <hellonedesing@gmail.com>
2023-03-18 20:52:50 +0800

    change app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="w-container flex a-center j-center column gap-20">
+    <header-component />
+    <div class="flex a-center j-center wrap w-100 gap-20">
+      <icon-card />
+    </div>
+  </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import headerComponent from './components/header.vue'
+import iconCard from './components/icon-card.vue'
+export default {
+  components: {
+    headerComponent,
+    iconCard
+  },
+  data() {
+    return {}
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
