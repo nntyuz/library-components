9c67a80d0ef4684725ffa965fd95d430a35aa77a
nnuyz <hellonedesing@gmail.com>
2023-03-18 22:07:35 +0800

    add button card

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <header-component />
     <div class="flex a-center j-center wrap w-100 gap-20">
       <icon-card />
+      <button-card />
     </div>
   </section>
 </template>
@@ -10,10 +11,13 @@
 <script>
 import headerComponent from './components/header.vue'
 import iconCard from './components/icon-card.vue'
+import buttonCard from './components/button-card.vue'
+
 export default {
   components: {
     headerComponent,
-    iconCard
+    iconCard,
+    buttonCard
   },
   data() {
     return {}
