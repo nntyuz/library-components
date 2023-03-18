81076953e87f35856e9647382b3e17b23e6b1616
nnuyz <hellonedesing@gmail.com>
2023-03-18 20:50:40 +0800

    add header

---
+++ src/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="header-component w-100 flex a-center j-center">
+    <h1 class="title">Vue Library Components</h1>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
