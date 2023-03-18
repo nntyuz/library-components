3a9aea9c89726eebb8b1ad569ad68fd71ac6ded4
nnuyz <hellonedesing@gmail.com>
2023-03-18 22:07:21 +0800

    add button component

---
+++ src/components/core/button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button :class="classes" @click="$emit('click')">
+    <vIcon v-if="icon" :name="icon" />
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'vButton',
+  props: {
+    type: {
+      type: String,
+      default: 'main'
+    },
+    icon: String,
+    iconPosition: {
+      type: String,
+      default: 'left'
+    },
+    size: {
+      type: String,
+      default: 'large'
+    },
+    disabled: Boolean
+  },
+  emits: ['click'],
+  computed: {
+    classes() {
+      const { type, icon, iconPosition, size, disabled } = this
+      let result = `button-component type-${type} size-${size} icon-position-${iconPosition}`
+      if (disabled) result += ' disabled'
+      if (icon) result += ' gap-20'
+      return result
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import vIcon from './components/core/icon.vue'
+import vButton from './components/core/button.vue'
 
 import '../src/assets/styles/main.scss'
 import '../src/assets/styles/reset.scss'
 
 const app = createApp(App)
-app.component('vIcon', vIcon)
+app.component('vIcon', vIcon).component('vButton', vButton)
 app.mount('#app')
