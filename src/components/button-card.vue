d1c962001c36712926349a00277a0fa5f4a03023
nnuyz <hellonedesing@gmail.com>
2023-03-22 14:44:29 +0800

    fix buttons

--- src/components/button-card.vue
+++ src/components/button-card.vue
@@ -3,7 +3,7 @@
     <h2>Buttons:</h2>
     <div class="flex a-start j-center wrap gap-20 w-100">
       <div class="flex j-center a-center column gap-20">
-        <h3>Type:</h3>
+        <h3>Types:</h3>
         <vButton type="main" size="large">Main Button</vButton>
         <vButton type="secondary" size="large">Secondary Button</vButton>
         <vButton type="text" size="large">Text Button</vButton>
@@ -16,11 +16,11 @@
         <vButton type="main" size="small">Small Button</vButton>
       </div>
       <div class="flex j-center a-center column gap-20">
-        <h3>Icon:</h3>
+        <h3>Icons:</h3>
         <vButton type="main" size="large" icon="chevronDown" />
-        <vButton type="secondary" size="large" icon="chevronDown">Left Icon Button</vButton>
-        <vButton type="main" size="large" icon="chevronDown" icon-position="right"
-          >Right Icon Button</vButton
+        <vButton type="secondary" size="large" icon="chevronDown">Right Icon Button</vButton>
+        <vButton type="main" size="large" icon="chevronDown" icon-position="left"
+          >Left Icon Button</vButton
         >
       </div>
     </div>
