5371e0efd9f0bddb31205ec377d4cccd290747a5
nnuyz <hellonedesing@gmail.com>
2023-03-22 16:21:29 +0800

    fix color

--- src/components/icons/success.vue
+++ src/components/icons/success.vue
@@ -3,13 +3,13 @@
     <g clip-path="url(#clip0_180_18473)">
       <path
         d="M8.00016 14.6667C11.6668 14.6667 14.6668 11.6667 14.6668 8.00001C14.6668 4.33334 11.6668 1.33334 8.00016 1.33334C4.3335 1.33334 1.3335 4.33334 1.3335 8.00001C1.3335 11.6667 4.3335 14.6667 8.00016 14.6667Z"
-        stroke="black"
+        stroke="currentColor"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M5.1665 8.00001L7.05317 9.88668L10.8332 6.11334"
-        stroke="black"
+        stroke="currentColor"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
