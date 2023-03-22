5b2e826b494e245eb7ce157a8163e8d74a23dc17
nnuyz <hellonedesing@gmail.com>
2023-03-22 23:09:01 +0800

    add multi-input component

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import vInput from './components/core/input.vue'
 import vRadio from './components/core/radio-button.vue'
 import vCheckbox from './components/core/checkbox.vue'
 import vTextarea from './components/core/textarea.vue'
+import vMultiInput from './components/core/multi-input.vue'
 
 import '../src/assets/styles/main.scss'
 import '../src/assets/styles/reset.scss'
@@ -19,4 +20,5 @@ app
   .component('vRadio', vRadio)
   .component('vCheckbox', vCheckbox)
   .component('vTextarea', vTextarea)
+  .component('vMultiInput', vMultiInput)
 app.mount('#app')
