cbcd359cf67c666d0c464d77ba485b7273b889f7
nnuyz <hellonedesing@gmail.com>
2023-03-22 16:22:27 +0800

    add input-card

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div class="flex a-center j-center wrap w-100 gap-20">
       <icon-card />
       <button-card />
+      <input-card />
     </div>
   </section>
 </template>
@@ -12,12 +13,13 @@
 import headerComponent from './components/header.vue'
 import iconCard from './components/icon-card.vue'
 import buttonCard from './components/button-card.vue'
-
+import inputCard from './components/input-card.vue'
 export default {
   components: {
     headerComponent,
     iconCard,
-    buttonCard
+    buttonCard,
+    inputCard
   },
   data() {
     return {}
---
+++ src/components/input-card.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="card flex a-center j-center column gap-20 w-100">
+    <h2>Inputs:</h2>
+    <div class="flex a-start j-center wrap gap-20 w-100">
+      <div class="flex j-center a-center column gap-20">
+        <h3>Password:</h3>
+        <vInput v-model="password" type="password" placeholder="Enter password" />
+      </div>
+      <div class="flex j-center a-center column gap-20">
+        <h3>Email:</h3>
+        <div class="flex a-center center gap-20">
+          <vInput
+            v-model="email"
+            type="email"
+            placeholder="Enter e-mail"
+            :error="isEmailInDataBase === false"
+            :success="isEmailInDataBase"
+            @keydown.enter="validateEmail"
+          />
+          <vButton type="main" size="medium" @click="validateEmail">Submit</vButton>
+        </div>
+      </div>
+      <div class="flex j-center a-center column gap-20">
+        <h3>Clear Value:</h3>
+        <vInput v-model="clearValue" type="text" placeholder="Right Icon Input" clear />
+      </div>
+      <div class="flex j-center a-center column gap-20">
+        <h3>Icons:</h3>
+        <vInput v-model="iconValue" type="text" placeholder="Placeholder" icon="plus" />
+        <vInput
+          v-model="iconValue"
+          type="text"
+          placeholder="Placeholder"
+          icon="search"
+          icon-position="left"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      iconValue: '',
+      clearValue: '',
+      password: '',
+      email: '',
+      isEmailInDataBase: null
+    }
+  },
+  methods: {
+    validateEmail() {
+      const re =
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      this.isEmailInDataBase = re.test(this.email)
+    }
+  }
+}
+</script>
