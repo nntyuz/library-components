633f94259cdac309f440a1b7d3a1159ffc7a1615
nnuyz <hellonedesing@gmail.com>
2023-03-22 18:15:19 +0800

    add radio-button component

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import App from './App.vue'
 import vIcon from './components/core/icon.vue'
 import vButton from './components/core/button.vue'
 import vInput from './components/core/input.vue'
+import vRadio from './components/core/radio-button.vue'
 
 import '../src/assets/styles/main.scss'
 import '../src/assets/styles/reset.scss'
 
 const app = createApp(App)
-app.component('vIcon', vIcon).component('vButton', vButton).component('vInput', vInput)
+app
+  .component('vIcon', vIcon)
+  .component('vButton', vButton)
+  .component('vInput', vInput)
+  .component('vRadio', vRadio)
 app.mount('#app')
